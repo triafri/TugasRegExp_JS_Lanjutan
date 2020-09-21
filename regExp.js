d76c01c3940aea69cfb4874b6e720b4cc223dc06
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-22 02:40:36 +0700

    Add files via upload

---
+++ regExp.js
@@ -0,0 +1,10 @@
+function panggilRegexp(value) {
+ let str = "abcdefghijklmnopqrstuvwxyz-A        B        C        D        E        F        G        H        I        J        K        L        M        N        O        P        Q        R        S        T        U        V        W        X        Y        Z";
+
+ //Tulis Code Jawaban Kamu Di Bawah ini
+ console.log(str.match(/[cC]/g))
+ console.log(str.match(/[kK]/g))
+ console.log(str.match(/[lL]/g))
+}
+
+panggilRegexp()
